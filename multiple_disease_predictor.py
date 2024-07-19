31bc3eec9da4b3360e90b4119f3f9ae61ced99e1
Vrush2004 <vrushalichaube@gmail.com>
2024-07-19 17:56:24 +0530

    multiple disease predicftor

--- multiple_disease_predictor.py
+++ multiple_disease_predictor.py
@@ -3,8 +3,15 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 #loading the saved models
-diabeties_model = pickle.load(open(r"C:/Users/Dell/Desktop/Multiple_Disease_Predictor/Saved Models/diabetes_model.sav"))
+diabeties_model = pickle.load(open(r"C:/Users/Dell/Desktop/Multiple_Disease_Predictor/Saved Models/diabetes_model.sav", 'rb'))
 
-heart_disease_model = pickle.load(open(r"C:\Users\Dell\Desktop\Multiple_Disease_Predictor\Saved Models\heart_disease_model.sav"))
+heart_disease_model = pickle.load(open(r"C:\Users\Dell\Desktop\Multiple_Disease_Predictor\Saved Models\heart_disease_model.sav", 'rb'))
 
-parkinsons_model = pickle.load(open(r"C:\Users\Dell\Desktop\Multiple_Disease_Predictor\Saved Models\parkinsons_model.sav"))
+parkinsons_model = pickle.load(open(r"C:\Users\Dell\Desktop\Multiple_Disease_Predictor\Saved Models\parkinsons_model.sav", 'rb'))
+
+#sidebar for navigate
+with st.sidebar:
+    selected = option_menu("Multiple Disease Prediction System",
+                           ['Diabetes Prediction','Heart Disease Prediction',
+                            'Parkinson Prediction'],
+                           default_index = 0)
